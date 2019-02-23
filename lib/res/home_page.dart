35360157737e1fa5f0f9d1d84622d96c031626ac
28JayG <jaygandhi.32@outlook.com>
2019-02-24 00:33:14 +0530

    abstracts the code

---
+++ lib/res/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+
+const Duration duration = Duration(
+  milliseconds: 600,
+);
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  double height;
+  double width;
+  Color color;
+  BoxShape boxShape = BoxShape.rectangle;
+  bool isCircle;
+
+  @override
+  void initState() {
+    super.initState();
+    height = 100.0;
+    width = 100.0;
+    color = Colors.blue;
+    isCircle = false;
+  }
+
+  _toggle() {
+    setState(() {
+      boxShape = isCircle ? BoxShape.circle : BoxShape.rectangle;
+      height = isCircle ? 400.0 : 100.0;
+      width = isCircle ? 400.0 : 100.0;
+      color = isCircle ? Colors.red : Colors.blue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: AnimatedContainer(
+          curve: Curves.fastOutSlowIn,
+          duration: duration,
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            color: color,
+//            shape: boxShape,
+          ),
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          isCircle = !isCircle;
+          _toggle();
+        },
+        tooltip: 'Animate',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+}
