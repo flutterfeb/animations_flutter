f6431580388e824f9d0c299435239f4094b06b34
28JayG <jaygandhi.32@outlook.com>
2019-02-26 14:39:48 +0530

    ready for tutorial

--- lib/res/home_page.dart
+++ lib/res/home_page.dart
@@ -15,28 +15,28 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   //TODO: [2] Declare and initialize animation and controller
   // {Hint: RotationTransition will take animation of type double for the turns}
-  Animation<double> animation;
-  AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(vsync: this, duration: duration)
-      ..addListener(() => setState(() {}));
-//Tip: you may also give this
-//    animation = CurvedAnimation(parent: controller, curve: Curves.elasticOut);
-    animation = Tween<double>(begin: -0.1, end: 0.1).animate(controller);
-    controller.forward();
-    controller.addStatusListener((AnimationStatus status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse();
-      }
-      if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
-  }
+//  Animation<double> animation;
+//  AnimationController controller;
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//
+//    controller = AnimationController(vsync: this, duration: duration)
+//      ..addListener(() => setState(() {}));
+////Tip: you may also give this a try
+////    animation = CurvedAnimation(parent: controller, curve: Curves.elasticOut);
+//    animation = Tween<double>(begin: -0.1, end: 0.1).animate(controller);
+//    controller.forward();
+//    controller.addStatusListener((AnimationStatus status) {
+//      if (status == AnimationStatus.completed) {
+//        controller.reverse();
+//      }
+//      if (status == AnimationStatus.dismissed) {
+//        controller.forward();
+//      }
+//    });
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,26 +50,26 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 // TODO:[1] Have your RotationTransition
         child: Stack(
           children: <Widget>[
-            RotationTransition(
-              turns: animation,
-              alignment: Alignment(0, -1),
-              child: Container(
-                height: 300,
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        width: 2,
-                        color: Colors.black,
-                      ),
-                    ),
-                    FlutterLogo(
-                      size: 150,
-                    ),
-                  ],
-                ),
-              ),
-            ),
+//            RotationTransition(
+////              turns: animation,
+////              alignment: Alignment(0, -1),
+////              child: Container(
+////                height: 300,
+////                child: Column(
+////                  children: <Widget>[
+////                    Expanded(
+////                      child: Container(
+////                        width: 2,
+////                        color: Colors.black,
+////                      ),
+////                    ),
+////                    FlutterLogo(
+////                      size: 150,
+////                    ),
+////                  ],
+////                ),
+////              ),
+//            ),
           ],
         ),
       ),
