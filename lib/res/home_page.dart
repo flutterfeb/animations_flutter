2d7fccfb5d7727204ffaaf970b067d2e86993b19
28JayG <jaygandhi.32@outlook.com>
2019-02-28 03:27:30 +0530

    ready for tutorial

--- lib/res/home_page.dart
+++ lib/res/home_page.dart
@@ -13,34 +13,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  //TODO:[2] Declare required variables
-//  bool isOpaque = true;
+  //TODO:[1.2] Declare required variables
+//  bool isPrevious = true;
 
   @override
   Widget build(BuildContext context) {
+    //TODO:[1.2]
+//    double height = MediaQuery.of(context).size.height;
+//    double width = MediaQuery.of(context).size.width;
+
     return new Scaffold(
       backgroundColor: Colors.white,
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: Center(
-          //TODO:[1] AnimatedOpacity and the properties
-//        child: AnimatedOpacity(
-//          opacity: isOpaque ? 1.0 : 0.4,
-//          duration: duration,
-//          child: Text(
-//            'CookyTech',
-//            style: TextStyle(
-//              fontWeight: FontWeight.bold,
-//              fontSize: 20,
+      body: Stack(
+        //TODO:[1.1] AnimatedPositioned and the properties
+        children: <Widget>[
+//          AnimatedPositioned(
+//            child: Container(
+//              color: Colors.blue,
 //            ),
+//            duration: duration,
+//            height: isPrevious ? 300 : 100,
+//            //child's height
+//            width: isPrevious ? 50 : 200,
+//            //child's width
+//            top: isPrevious ? height * 0.5 : height * 0.01,
+//            left: isPrevious ? width * 0.05 : width * 0.4,
 //          ),
-//        ),
-          ),
+
+          //OR
+          //TODO:[3.1] AnimatedPositioned of another kind and the properties
+//          AnimatedPositioned.fromRect(
+//              duration: duration,
+//              child: Container(
+//                color: Colors.yellow,
+//              ),
+//              rect: Rect.fromLTWH(
+//                isPrevious ? width * 0.4 : width * 0.05,
+//                //left
+//                isPrevious ? height * 0.01 : height * 0.5,
+//                //top
+//                isPrevious ? 200 : 50,
+//                //width
+//                isPrevious ? 100 : 300,
+//                //height
+//              )),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          //TODO:[3] change what's needed to be and set the state
-//          isOpaque = !isOpaque;
+          //TODO:[2] change what's needed to be and set the state
+//          isPrevious = !isPrevious;
 //          setState(() {});
         },
         child: Icon(Icons.play_arrow),
