6892bdd6af8b11d047c84ce3da294c467fd2646a
biswa1751 <suraj53028@gmail.com>
2019-03-01 01:05:27 +0530

    Ready For Tutorial(Updated)

--- lib/res/secondary_page.dart
+++ lib/res/secondary_page.dart
@@ -1,44 +1,25 @@
 //TODO:[3] Okay so you made the hero navigate but where is the page where it will 
 //         navigate to, you don't wanna fall in red sea of yellow errors.
 
-//import 'package:flutter/material.dart';
-//import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-//
-//class SecondaryPage extends StatelessWidget {
+// import 'package:flutter/material.dart';
+// class SecondaryPage extends StatelessWidget {
 //  SecondaryPage({Key key, this.title}) : super(key: key);
 //  final String title;
-//
+
 //  @override
 //  Widget build(BuildContext context) {
 //    return new Scaffold(
 //      appBar: new AppBar(
 //        title: new Text(title),
 //      ),
-//      body: Stack(
-//        children: <Widget>[
-//
-//          Align(
-//            alignment: Alignment.bottomLeft,
-//            child: Hero(
+//      body: Center(
+//        child:  Hero(
 //              tag: 'flutter logo',
 //              child: FlutterLogo(
 //                size: 200.0,
 //              ),
 //            ),
-//          ),
-//          Align(
-//            alignment: Alignment.topRight,
-//            child: Hero(
-//              tag: 'some logo',
-//              child: Icon(
-//                MdiIcons.firebase,
-//                size: 200.0,
-//                color: Colors.yellow,
-//              ),
-//            ),
-//          ),
-//        ],
-//      ),
+//      )
 //    );
 //  }
-//}
+// }
